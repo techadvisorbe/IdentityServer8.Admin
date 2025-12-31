074e09ed5b32bbf8ff15fd145625ecb979bc093a
Abdelilah El mansouri <contact@techadvisor.be>
2025-12-31 14:56:44 +0100

    fix package

--- templates/template-publish/content/src/Skoruba.IdentityServer8.Admin.BusinessLogic/Dtos/Configuration/ApiScopePropertyDto.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Skoruba.IdentityServer8.Admin.BusinessLogic.Dtos.Configuration
-{
-    public class ApiScopePropertyDto
-    {
-        public int Id { get; set; }
-        public string Key { get; set; }
-        public string Value { get; set; }
-    }
-}

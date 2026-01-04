5788281da7183d907da46fa3762d917c1df14040
Abdelilah El mansouri <contact@techadvisor.be>
2026-01-04 15:54:09 +0100

    add event classes for log deletion and request handling

---
+++ src/TechAdvisor.IdentityServer8.Admin.BusinessLogic/Events/Log/LogsDeletedEvent.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using TechAdvisor.AuditLogging.Events;
+
+namespace TechAdvisor.IdentityServer8.Admin.BusinessLogic.Events.Log
+{
+    public class LogsDeletedEvent : AuditEvent
+    {
+        public DateTime DeleteOlderThan { get; set; }
+
+        public LogsDeletedEvent(DateTime deleteOlderThan)
+        {
+            DeleteOlderThan = deleteOlderThan;
+        }
+    }
+}
---
+++ src/TechAdvisor.IdentityServer8.Admin.BusinessLogic/Events/Log/LogsRequestedEvent.cs
@@ -0,0 +1,8 @@
+﻿using TechAdvisor.AuditLogging.Events;
+
+namespace TechAdvisor.IdentityServer8.Admin.BusinessLogic.Events.Log
+{
+    public class LogsRequestedEvent : AuditEvent
+    {
+    }
+}
